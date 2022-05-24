341a5af64b8de0d952f7e39e632709f1618fd696
Gabriel Lima <gabriellimamoraes@gmail.com>
2022-05-23 21:50:52 -0300

    frontend

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
-
  
     $('#scrolTop, #scrolTop2, #scrolTop3, #scrolTop4').click(function(){
         $('html, body').animate({scrollTop:0}, 'slow');
     });
-    
+
     if($('#error_name:visible')){
         setTimeout(function(){
             $('#error_name').fadeOut();;
@@ -12,3 +11,17 @@ $(document).ready(function() {
     }
 
 });
+
+function createTopic() {
+    document.getElementById("topic-create").style.display = "block";
+    document.getElementById("topic-home").style.display = "none";
+    document.getElementById("topic-sucess").style.display = "none";
+    document.getElementById("obfuscate").style.display = "none";
+}
+
+function sendTopic() {
+    document.getElementById("topic-sucess").style.display = "block";
+    document.getElementById("obfuscate").style.display = "block";
+    document.getElementById("topic-home").style.display = "none";
+    document.getElementById("topic-create").style.display = "none";
+}
